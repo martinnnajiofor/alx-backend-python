de6bc33ddad14d8ea566de08889599551e077007
martinnnajiofor <martinnnajiofor@yahoo.com>
2023-02-06 04:36:47 -0800

    0x01-python_async_function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,31 +1,25 @@
 #!/usr/bin/env python3
 """
-Import wait_random from the previous python file that
-you’ve written and write an async routine called wait_n
-that takes in 2 int arguments (in this order):
-n and max_delay. You will spawn wait_random n times with
-the specified max_delay
+Import wait_random from the previous python file that you’ve written and write an async routine called wait_n that takes in 2 int arguments (in this order): n and max_delay. You will spawn wait_random n times with the specified max_delay.
+
+wait_n should return the list of all the delays (float values). The list of the delays should be in ascending order without using sort() because of concurrency.
 """
 import asyncio
 from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_random
+
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    wait_n should return the list of all the delays
-    (float values). The list of the delays should be
-    in ascending order without using sort() because
-    of concurrency
-    """
-    tasks = []
-    result = []
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+   
+    results = []
+    promises = []
+    for _ in range(0, n):
+        task = asyncio.create_task(wait_random(max_delay))
+        task.add_done_callback(lambda x: results.append(x.result()))
+        promises.append(task)
 
-    for i in range(n):
-        tasks.append(asyncio.create_task(wait_random(max_delay)))
+    for promise in promises:
+        await promise
 
-    for task in asyncio.as_completed(tasks):
-        result.append(await task)
-
-    return result
-
+    return results
